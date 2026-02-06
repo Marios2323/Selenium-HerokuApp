503bcc9d649aa380c13f70a88b3c1fb8382c4455
Michael <michaelgalliko@gmail.com>
2026-02-06 12:39:07 +0200

    Modify LoginTest.java

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,10 +1,8 @@
 package tests;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,6 +13,7 @@ import org.testng.annotations.Test;
 import pages.LoginPage;
 
 import java.time.Duration;
+
 public class LoginTest {
 
     private static final Logger log = LoggerFactory.getLogger(LoginTest.class);
@@ -33,26 +32,15 @@ public class LoginTest {
 
         LoginPage loginPage = new LoginPage(driver);
 
-        driver.get("https://the-internet.herokuapp.com/login");
+        loginPage.navigateToLoginPage();
 
-        Assert.assertTrue(
-                wait.until(ExpectedConditions
-                                .visibilityOfElementLocated(By.xpath("//h2[text()='Login Page']")))
-                        .isDisplayed(),
-                "Login Page is not visible"
-        );
+        Assert.assertTrue(loginPage.isLoginSuccessful(), "Login was not successful");
 
         loginPage.typeUsername("tomsmith");
         loginPage.typePassword("SuperSecretPassword!");
         loginPage.pressLoginButton();
 
-        Assert.assertTrue(
-                wait.until(ExpectedConditions
-                                .visibilityOfElementLocated(By.id("flash")))
-                        .getText().contains("You logged into a secure area!"),
-                "Login failed"
-        );
-        Assert.assertTrue(driver.getCurrentUrl().contains("/secure"));
+        Assert.assertTrue(loginPage.isLoginSecure(), "Page is not a secure page!");
     }
 
     @AfterMethod
