727fd300ae90136c351afe4282da986aea0ddaee
Michael <michaelgalliko@gmail.com>
2026-02-08 08:51:10 +0200

    Add negative tests LoginTest.java

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -15,13 +15,11 @@ import java.time.Duration;
 public class LoginTest {
 
     WebDriver driver;
-    WebDriverWait wait;
 
     @BeforeMethod
     public void setup() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
-        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
     }
 
     @Test
@@ -31,15 +29,51 @@ public class LoginTest {
 
         loginPage.navigateToLoginPage();
 
+        //Verifies that the login page is accessible
+        Assert.assertTrue(loginPage.isLoginPageVisible(), "Login page is not available");
+
         loginPage.typeUsername("tomsmith");
         loginPage.typePassword("SuperSecretPassword!");
         loginPage.pressLoginButton();
 
-        //Verifies that the login page is accessible
-        Assert.assertTrue(loginPage.isLoginPageVisible(), "Login was not successful");
-
         //Verifies the login page is secure
         Assert.assertTrue(loginPage.isLoginSecure(), "Page is not a secure page!");
+    }
+
+    @Test
+    public void wrongUsernameTest() {
+
+        LoginPage loginPage = new LoginPage(driver);
+
+        loginPage.navigateToLoginPage();
+
+        //Verifies that the login page is accessible
+        Assert.assertTrue(loginPage.isLoginPageVisible(), "Login page is not available");
+
+        loginPage.typeUsername("wrongUsername");
+        loginPage.typePassword("SuperSecretPassword!");
+        loginPage.pressLoginButton();
+
+        //Verifies the login page is secure
+        Assert.assertTrue(loginPage.isWrongUsernameMessageDisplayed(), "Correct message is not shown");
+    }
+
+    @Test
+    public void wrongPasswordTest() {
+
+        LoginPage loginPage = new LoginPage(driver);
+
+        loginPage.navigateToLoginPage();
+
+        //Verifies that the login page is accessible
+        Assert.assertTrue(loginPage.isLoginPageVisible(), "Correct message is not shown");
+
+        loginPage.typeUsername("tomsmith");
+        loginPage.typePassword("wrongPassword");
+        loginPage.pressLoginButton();
+
+        //Verifies the login page is secure
+        Assert.assertTrue(loginPage.isWrongPasswordMessageDisplayed(), "Correct message is not shown");
     }
 
     @AfterMethod
