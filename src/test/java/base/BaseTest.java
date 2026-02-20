3b85796403ba80e3e8c14fe15bec7db86d71b7c6
Michael <michaelgalliko@gmail.com>
2026-02-20 09:14:02 +0200

    feat: add parallel cross-browser support with Edge offline driver
    - Updated DriverFactory to support Chrome, Firefox, and Edge
    - Configured Edge to use local driver executable for offline usage
    - Updated BaseTest to accept browser parameter for parallel execution
    - Added testng.xml suite for running tests in parallel across browsers

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -4,12 +4,15 @@ import drivers.DriverFactory;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import config.ConfigReader;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 
 public class BaseTest {
 
     @BeforeMethod
-    public void setup() {
-        DriverFactory.initDriver(ConfigReader.getBrowser());
+    @Parameters({"browser"})
+    public void setup(@Optional("edge") String browser) {
+        DriverFactory.initDriver(browser);
         DriverFactory.getDriver().get(ConfigReader.getBaseUrl());
     }
 
--- src/test/java/drivers/DriverFactory.java
+++ src/test/java/drivers/DriverFactory.java
@@ -2,12 +2,13 @@ package drivers;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.WebDriver;
 
 public class DriverFactory {
 
-    private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+    private final static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
 
     public static void initDriver(String browser) {
 
@@ -22,6 +23,13 @@ public class DriverFactory {
             case "firefox":
                 WebDriverManager.firefoxdriver().setup();
                 webDriver = new FirefoxDriver();
+                break;
+
+            case "edge":
+                System.setProperty("webdriver.edge.driver",
+                        "C:\\Users\\Michael\\IdeaProjects\\Selenium-HerokuApp\\EdgeDrivers\\msedgedriver.exe");
+
+                webDriver = new EdgeDriver();
                 break;
 
             default:
